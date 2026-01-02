83a2036d604172da77a6af7c306ce56cce1e4ac0
blueberrys2635 <blueberrys2635@atomicmail.io>
2026-01-01 18:12:19 -0800

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -9,3 +9,9 @@ clear
 mkdir dockercom
 cd dockercom
 clear
+wget https://raw.githubusercontent.com/blueberrys2635/glowing-octo-guide/refs/heads/main/compose.yml
+clear
+ls
+clear
+sudo docker-compose up -d
+clear
